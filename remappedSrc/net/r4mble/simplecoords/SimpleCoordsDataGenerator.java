2c41ef87606c62333091ab8ac2b9f48094afa776
R4mBLe_ <misharad2008@gmail.com>
2024-11-25 20:07:34 +0300

    add version to mc1.20.5-1.20.6

---
+++ remappedSrc/net/r4mble/simplecoords/SimpleCoordsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.r4mble.simplecoords;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleCoordsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
